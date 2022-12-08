2734f6148b1f22deef2d7893aadfbe673bf9e471
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-12-08 21:33:48 +0700

    add auth

--- src/components/layouts/LoginPage.tsx
+++ src/components/layouts/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useAuth } from "../../utils/hook/useAuth";
 
 interface IProps {
   setIsLogin: (value: boolean) => void;
@@ -6,6 +7,7 @@ interface IProps {
 
 function LoginPage(props: IProps) {
   const { setIsLogin } = props;
+  const auth = useAuth();
   const [userEmail, setUserEmail] = useState<string>();
   const [userPassword, setUserPassword] = useState<string>();
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -13,13 +15,18 @@ function LoginPage(props: IProps) {
     console.log("submited", userEmail, userPassword);
     // call API auth LOGIN
     // .then (data.json())
-    // .then(setIsLogin(true); localStorage.setItem('token' , res.token)) 
+    // .then(setIsLogin(true); localStorage.setItem('token' , res.token))
     // .catch
 
     if (userEmail === "admin" && userPassword === "admin") {
       setIsLogin(true);
     } else {
       console.log(0);
+    }
+  };
+  const login = () => {
+    if (userEmail && userPassword) {
+      auth.signIn(userEmail, userPassword, () => {});
     }
   };
   return (
