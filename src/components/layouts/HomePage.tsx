55f51f579d33915be7a105d563b9e443551c0995
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-12-08 21:28:17 +0700

    add auth

--- src/components/layouts/HomePage.tsx
+++ src/components/layouts/HomePage.tsx
@@ -13,43 +13,78 @@ function HomePage({}: Props) {
   const { users } = useSelector((state: State) => state.user);
   useEffect(() => {
     // CALL API  get list Users
-    dispatch(getUsersAsync())
+    dispatch(getUsersAsync());
   }, []);
 
   const handleDelete = (id: string | undefined) => {
-    dispatch(deleteUserAsync(id))
+    dispatch(deleteUserAsync(id));
   };
 
   return (
-    <table className="table table-light table-responsive table-striped table-hover w-75 m-auto border border-secondary">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user: IUser) => {
-          return (
-            <tr key={user?.id}>
-              <th scope="row" onClick={() => navigate(`/${user?.id}`)}>
-                {user?.id}
-              </th>
-              <td onClick={() => navigate(`/${user?.id}`)}>{user?.name}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(user?.id)}
-                >
-                  Delete
-                </button>
-              </td>
+    <div className="row">
+      <div className="col-12 col-md-6 col-xl-8">
+        <table className="table table-light table-responsive table-striped table-hover w-75 m-auto border border-secondary">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col"></th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {users.map((user: IUser) => {
+              return (
+                <tr key={user?.id}>
+                  <th scope="row" onClick={() => navigate(`/${user?.id}`)}>
+                    {user?.id}
+                  </th>
+                  <td onClick={() => navigate(`/${user?.id}`)}>{user?.name}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(user?.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="col-12 col-md-6 col-xl-4">
+        <table className="table table-light table-responsive table-striped table-hover w-75 m-auto border border-secondary">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user: IUser) => {
+              return (
+                <tr key={user?.id}>
+                  <th scope="row" onClick={() => navigate(`/${user?.id}`)}>
+                    {user?.id}
+                  </th>
+                  <td onClick={() => navigate(`/${user?.id}`)}>{user?.name}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(user?.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
---
+++ src/config/env.js
@@ -0,0 +1,3 @@
+export const API_URL = 'https://63284e93a2e90dab7bdd0fd7.mockapi.io/api/v1/';
+export const WIDTH_DEVICE = 375;
+export const HEIGHT_DEVICE = 667;
